eb30c22fe4357a68807cf4c3c48fa6323ccfa50c
Albert Krewinkel <albert@zeitkraut.de>
2021-10-24 15:51:55 +0200

    Update to hslua-2.0

--- test/test-hslua-module-doclayout.hs
+++ test/test-hslua-module-doclayout.hs
@@ -1,31 +1,30 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications  #-}
 {-|
 Module      : Main
-Copyright   : © 2020 Albert Krewinkel
+Copyright   : © 2020-2021 Albert Krewinkel
 License     : MIT
 Maintainer  : Albert Krewinkel <albert+hslua@zeitkraut.de>
-Stability   : alpha
-Portability : Requires language extensions ForeignFunctionInterface,
-              OverloadedStrings.
 
 Tests for the `doclayout` Lua module.
 -}
 module Main (main) where
 
 import Control.Monad (void)
-import Foreign.Lua (Lua)
-import Foreign.Lua.Module.DocLayout (preloadModule, pushModule)
+import HsLua (Lua)
+import Foreign.Lua.Module.DocLayout (documentedModule)
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import Test.Tasty.HUnit (assertEqual, testCase)
 import Test.Tasty.Lua (translateResultsFromFile)
 
-import qualified Foreign.Lua as Lua
+import qualified HsLua as Lua
 
 main :: IO ()
 main = do
-  luaTestResults <- Lua.run $ do
+  luaTestResults <- Lua.run @Lua.Exception $ do
     Lua.openlibs
-    Lua.requirehs "doclayout" (void pushModule)
+    Lua.registerModule documentedModule
+    Lua.pop 1 -- pop module table
     translateResultsFromFile "test/test-doclayout.lua"
   defaultMain $ testGroup "hslua-module-doclayout" [tests, luaTestResults]
 
@@ -33,20 +32,20 @@ main = do
 tests :: TestTree
 tests = testGroup "HsLua doclayout module"
   [ testCase "module can be pushed to the stack" $
-      Lua.run (void pushModule)
+      Lua.run (void (Lua.pushModule documentedModule) :: Lua ())
 
   , testCase "module can be added to the preloader" . Lua.run $ do
       Lua.openlibs
-      preloadModule "doclayout"
+      Lua.preloadModule documentedModule
       assertEqual' "function not added to preloader" Lua.TypeFunction =<< do
         Lua.getglobal' "package.preload.doclayout"
         Lua.ltype (-1)
 
-  , testCase "module can be loaded via preloader" . Lua.run $ do
+  , testCase "module can be loaded as `layout`" . Lua.run $ do
       Lua.openlibs
-      preloadModule "doclayout"
+      Lua.preloadModuleWithName documentedModule "layout"
       assertEqual' "loading the module fails " Lua.OK =<<
-        Lua.dostring "require 'doclayout'"
+        Lua.dostring "require 'layout'"
   ]
 
 assertEqual' :: (Show a, Eq a) => String -> a -> a -> Lua ()
